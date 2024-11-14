f71309f5118ca7eb5b2ab7e911ea6ff473e72fb1
Tajohnson245 <tajohnson245@outlook.com>
2024-11-14 18:42:26 -0500

    Create separate component for adding transactions

--- DBMSProject/frontend/src/components/Transactions.jsx
+++ DBMSProject/frontend/src/components/Transactions.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import AddTransaction from "./AddTransactions";
 
 const Transactions = () => {
   const [allTransactions, setAllTransactions] = useState([]);
@@ -10,21 +11,8 @@ const Transactions = () => {
     query: "",
   });
 
-  const [formData2, setFormData2] = useState({
-    Transaction_id: "",
-    Account_id: "",
-    Transaction_type: "",
-    Transaction_date: "",
-    Description: "",
-    Amount: "",
-    Balance: "",
-    Category: "",
-    Category_id: "",
-  });
-
   useEffect(() => {
     const timer = setTimeout(() => {
-      setStatusMessageAdd(null);
       setStatusMessageSearch(null);
     }, 3000);
 
@@ -84,43 +72,6 @@ const Transactions = () => {
     }
   };
 
-  const addTransactionHandleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch("http://localhost:3000/api/addTransaction", {
-        method: "POST", // Specify POST method
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData2), // Send form data as JSON in the request body
-      });
-      if (!response.ok) {
-        throw new Error("Failed to add transaction.");
-      }
-      const data = await response.json();
-      setStatusMessageAdd({
-        type: "success",
-        text: "Transaction added successfully.",
-      });
-      setFormData2({
-        Transaction_id: "",
-        Account_id: "",
-        Transaction_type: "",
-        Transaction_date: "",
-        Description: "",
-        Amount: "",
-        Balance: "",
-        Category: "",
-        Category_id: "",
-      });
-    } catch (err) {
-      setStatusMessageAdd({
-        type: "error",
-        text: err.message,
-      });
-    }
-  };
-
   const deleteTransaction = async () => {
     const response = await fetch(
       "http://localhost:3000/api/deleteTransactions",
@@ -159,33 +110,6 @@ const Transactions = () => {
     });
   };
 
-  const handleChange2 = (e) => {
-    const { name, value } = e.target;
-
-    if (
-      name === "Transaction_date" &&
-      value &&
-      /^\d{2}\/\d{2}\/\d{4}$/.test(value)
-    ) {
-      const [month, day, year] = value.split("/");
-
-      const formattedDate = `${year}-${month.padStart(2, "0")}-${day.padStart(
-        2,
-        "0"
-      )}`;
-
-      setFormData2({
-        ...formData2,
-        [name]: formattedDate,
-      });
-    } else {
-      setFormData2({
-        ...formData2,
-        [name]: value,
-      });
-    }
-  };
-
   const handleSelect = (Transaction_id) => {
     setSelectedTransactions((prevSelected) =>
       prevSelected.includes(Transaction_id)
@@ -197,136 +121,6 @@ const Transactions = () => {
   return (
     <div className="flex flex-col">
       <div className="flex flex-col space-y-5 mb-5">
-        <form
-          className="bg-white p-5 rounded-xl shadow-md w-full"
-          onSubmit={addTransactionHandleSubmit}
-        >
-          <div className="grid grid-cols-2 gap-4">
-            <div>
-              <label>
-                Transaction ID:
-                <input
-                  className="border rounded p-2 block w-full mt-1"
-                  type="text"
-                  name="Transaction_id"
-                  value={formData2.Transaction_id}
-                  onChange={handleChange2}
-                />
-              </label>
-            </div>
-            <div>
-              <label>
-                Account ID:
-                <input
-                  className="border rounded p-2 block w-full mt-1"
-                  type="text"
-                  name="Account_id"
-                  value={formData2.Account_id}
-                  onChange={handleChange2}
-                />
-              </label>
-            </div>
-            <div>
-              <label>
-                Transaction Type:
-                <input
-                  className="border rounded p-2 block w-full mt-1"
-                  type="text"
-                  name="Transaction_type"
-                  value={formData2.Transaction_type}
-                  onChange={handleChange2}
-                />
-              </label>
-            </div>
-            <div>
-              <label>
-                Transaction Date:
-                <input
-                  className="border rounded p-2 block w-full mt-1"
-                  type="date"
-                  name="Transaction_date"
-                  value={formData2.Transaction_date}
-                  onChange={handleChange2}
-                />
-              </label>
-            </div>
-            <div>
-              <label>
-                Description:
-                <input
-                  className="border rounded p-2 block w-full mt-1"
-                  type="text"
-                  name="Description"
-                  value={formData2.Description}
-                  onChange={handleChange2}
-                />
-              </label>
-            </div>
-            <div>
-              <label>
-                Amount:
-                <input
-                  className="border rounded p-2 block w-full mt-1"
-                  type="text"
-                  name="Amount"
-                  value={formData2.Amount}
-                  onChange={handleChange2}
-                />
-              </label>
-            </div>
-            <div>
-              <label>
-                Balance:
-                <input
-                  className="border rounded p-2 block w-full mt-1"
-                  type="text"
-                  name="Balance"
-                  value={formData2.Balance}
-                  onChange={handleChange2}
-                />
-              </label>
-            </div>
-            <div>
-              <label>
-                Category:
-                <input
-                  className="border rounded p-2 block w-full mt-1"
-                  type="text"
-                  name="Category"
-                  value={formData2.Category}
-                  onChange={handleChange2}
-                />
-              </label>
-            </div>
-            <div>
-              <label>
-                Category ID:
-                <input
-                  className="border rounded p-2 block w-full mt-1"
-                  type="text"
-                  name="Category_id"
-                  value={formData2.Category_id}
-                  onChange={handleChange2}
-                />
-              </label>
-            </div>
-          </div>
-          <button type="submit" className="bg-teal-500 text-white p-3 mt-3">
-            Add Transaction
-          </button>
-          {statusMessageAdd && (
-            <div
-              className={
-                statusMessageAdd.type === "error"
-                  ? "text-red-500"
-                  : "text-green-500"
-              }
-            >
-              {statusMessageAdd.text}
-            </div>
-          )}
-        </form>
-
         <form
           className="bg-white rounded-xl p-5 w-full shadow-md"
           onSubmit={searchTransactionsHandleSubmit}
@@ -360,6 +154,7 @@ const Transactions = () => {
             </div>
           )}
         </form>
+        <AddTransaction />
       </div>
 
       <table className="w-full border-collapse bg-white rounded-lg shadow-lg">
