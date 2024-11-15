2caf340c9b02604925ec16ee49dc8f235d3a9266
Tajohnson245 <tajohnson245@outlook.com>
2024-11-14 19:50:36 -0500

    fix button and status handling

--- DBMSProject/frontend/src/components/Transactions.jsx
+++ DBMSProject/frontend/src/components/Transactions.jsx
@@ -37,15 +37,21 @@ const Transactions = () => {
   const searchTransactionsHandleSubmit = async (e) => {
     e.preventDefault();
 
+    const query = formData.query.trim();
+
+    if (!query) {
+      setStatusMessageSearch({
+        type: "error",
+        text: "Please enter a search term.",
+      });
+      return;
+    }
+
     const queryParams = new URLSearchParams({
       query: formData.query.trim(),
     });
 
     try {
-      console.log(
-        `http://localhost:3000/api/searchTransactions?${queryParams.toString()}`
-      );
-
       const response = await fetch(
         `http://localhost:3000/api/searchTransactions?${queryParams.toString()}`
       );
@@ -53,7 +59,6 @@ const Transactions = () => {
         throw new Error("Failed to fetch transactions.");
       }
       const data = await response.json();
-      console.log(data);
       setTransactions(data);
       setStatusMessageSearch({
         type: "success",
@@ -119,130 +124,136 @@ const Transactions = () => {
   const toggleAddTransaction = () => setIsOpen((prev) => !prev);
 
   return (
-    <div className="flex flex-col">
-      <div className="flex items-center space-x-3 mb-5">
-        <form
-          className="bg-white rounded-xl p-5 w-full shadow-md"
-          onSubmit={searchTransactionsHandleSubmit}
-        >
-          <div>
-            <label>
-              Search:
-              <input
-                className="border rounded p-2 block w-full mt-1"
-                type="text"
-                name="query"
-                value={formData.query}
-                onChange={handleChange}
-              />
-            </label>
-          </div>
-          <div className="flex flex-row">
+    <>
+      <div className="flex flex-col">
+        <div className="flex items-center space-x-3 mb-5">
+          <form
+            className="bg-white rounded-xl p-5 w-full shadow-md"
+            onSubmit={searchTransactionsHandleSubmit}
+          >
             <div>
-              <button type="submit" className="bg-teal-500 text-white p-3 mt-3">
-                Search Transactions
-              </button>
+              <label>
+                Search:
+                <input
+                  className="border rounded p-2 block w-full mt-1"
+                  type="text"
+                  name="query"
+                  value={formData.query}
+                  onChange={handleChange}
+                />
+              </label>
             </div>
-          </div>
+            <div className="flex flex-row">
+              <div>
+                <button
+                  type="submit"
+                  className="bg-teal-500 text-white p-3 mt-3"
+                >
+                  Search Transactions
+                </button>
+              </div>
+              <div className="pl-4 pt-4">
+                <button
+                  type="button"
+                  className="bg-teal-500 text-white rounded-full px-4 py-2 flex items-center justify-center text-base font-semibold"
+                  onClick={toggleAddTransaction}
+                >
+                  Add Transactions
+                </button>
+              </div>
+            </div>
 
-          {statusMessageSearch && (
-            <div
-              className={
-                statusMessageSearch.type === "error"
-                  ? "text-red-500"
-                  : "text-green-500"
-              }
-            >
-              {statusMessageSearch.text}
-            </div>
-          )}
-        </form>
-        <div className="pl-4 pt-4">
-          <button
-            className="bg-teal-500 text-white rounded-full w-10 h-10 flex items-center justify-center text-lg"
-            onClick={toggleAddTransaction}
-          >
-            +
-          </button>
+            {statusMessageSearch && (
+              <div
+                className={
+                  statusMessageSearch.type === "error"
+                    ? "text-red-500"
+                    : "text-green-500"
+                }
+              >
+                {statusMessageSearch.text}
+              </div>
+            )}
+          </form>
         </div>
-      </div>
 
-      {isOpen && (
-        <div
-          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
-          onClick={toggleAddTransaction} // Close modal when clicking outside
-        >
+        {isOpen && (
           <div
-            className="bg-white p-5 rounded-xl shadow-lg w-11/12 md:w-1/2 relative"
-            onClick={(e) => e.stopPropagation()} // Prevent modal close on content click
+            className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
+            onClick={toggleAddTransaction} // Close modal when clicking outside
           >
-            <button
-              className="absolute top-2 right-2 bg-red-500 text-white rounded-full w-8 h-8 flex items-center justify-center"
-              onClick={toggleAddTransaction}
+            <div
+              className="bg-white p-5 rounded-xl shadow-lg w-11/12 md:w-1/2 relative"
+              onClick={(e) => e.stopPropagation()} // Prevent modal close on content click
             >
-              &times;
-            </button>
-            <AddTransaction />
-          </div>
-        </div>
-      )}
-
-      <table className="w-full border-collapse bg-white rounded-lg shadow-lg">
-        <thead>
-          <tr className="bg-[#22577A] text-white">
-            <th className="p-2">Transaction Id</th>
-            <th className="p-2">Account Id</th>
-            <th className="p-2">Transaction Type</th>
-            <th className="p-2">Transaction Date</th>
-            <th className="p-2">Description</th>
-            <th className="p-2">Amount</th>
-            <th className="p-2">Balance</th>
-            <th className="p-2">Category</th>
-            <th className="p-2">Category Id</th>
-            <th className="p-2">
               <button
-                className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-2 rounded"
-                onClick={deleteTransaction}
+                className="absolute top-2 right-2 bg-red-500 text-white rounded-full w-8 h-8 flex items-center justify-center"
+                onClick={toggleAddTransaction}
               >
-                Delete
+                &times;
               </button>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {transactions.length > 0 ? (
-            transactions.map((transaction) => (
-              <tr
-                key={transaction.Transaction_id}
-                className="hover:bg-[#38A3A5]"
-              >
-                <td className="p-2 border">{transaction.Transaction_id}</td>
-                <td className="p-2 border">{transaction.Account_id}</td>
-                <td className="p-2 border">{transaction.Transaction_type}</td>
-                <td className="p-2 border">{transaction.Transaction_date}</td>
-                <td className="p-2 border">{transaction.Description}</td>
-                <td className="p-2 border">{transaction.Amount}</td>
-                <td className="p-2 border">{transaction.Balance}</td>
-                <td className="p-2 border">{transaction.Category}</td>
-                <td className="p-2 border">{transaction.Category_id}</td>
-                <td className="p-2 pl-8 border">
-                  <input
-                    type="checkbox"
-                    onChange={() => handleSelect(transaction.Transaction_id)}
-                  />
+              <AddTransaction />
+            </div>
+          </div>
+        )}
+
+        <table className="w-full border-collapse bg-white rounded-lg shadow-lg">
+          <thead>
+            <tr className="bg-[#22577A] text-white">
+              <th className="p-2">Transaction Id</th>
+              <th className="p-2">Account Id</th>
+              <th className="p-2">Transaction Type</th>
+              <th className="p-2">Transaction Date</th>
+              <th className="p-2">Description</th>
+              <th className="p-2">Amount</th>
+              <th className="p-2">Balance</th>
+              <th className="p-2">Category</th>
+              <th className="p-2">Category Id</th>
+              <th className="p-2">
+                <button
+                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-2 rounded"
+                  onClick={deleteTransaction}
+                >
+                  Delete
+                </button>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.length > 0 ? (
+              transactions.map((transaction) => (
+                <tr
+                  key={transaction.Transaction_id}
+                  className="hover:bg-[#38A3A5]"
+                >
+                  <td className="p-2 border">{transaction.Transaction_id}</td>
+                  <td className="p-2 border">{transaction.Account_id}</td>
+                  <td className="p-2 border">{transaction.Transaction_type}</td>
+                  <td className="p-2 border">{transaction.Transaction_date}</td>
+                  <td className="p-2 border">{transaction.Description}</td>
+                  <td className="p-2 border">{transaction.Amount}</td>
+                  <td className="p-2 border">{transaction.Balance}</td>
+                  <td className="p-2 border">{transaction.Category}</td>
+                  <td className="p-2 border">{transaction.Category_id}</td>
+                  <td className="p-2 pl-8 border">
+                    <input
+                      type="checkbox"
+                      onChange={() => handleSelect(transaction.Transaction_id)}
+                    />
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="10" className="text-center p-3">
+                  No transactions found.
                 </td>
               </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan="10" className="text-center p-3">
-                No transactions found.
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    </div>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
